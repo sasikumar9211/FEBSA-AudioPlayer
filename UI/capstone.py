c2950aa5aa7a147f3b969ee8f9128b70ae4be8b4
sasikumar9211 <sasikumar.balasubramanian92@gmail.com>
2021-11-06 16:34:50 +0530

    Soft Voting

--- UI/capstone.py
+++ UI/capstone.py
@@ -3,6 +3,8 @@ import base64
 import io
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import cv2
 from PIL import Image
 from flask_cors import CORS 
@@ -26,8 +28,8 @@ def load_image(img_path):
       
 @app.route('/image',methods = ['POST'])  
 def getImage():
-    EMOTIONS_LIST = ["Angry", "Disgust","Fear", "Happy","Neutral", "Sad","Surprise"]
-    print(request.get_json())
+    #EMOTIONS_LIST = ["Angry", "Disgust","Fear", "Happy","Neutral", "Sad","Surprise"]
+    #print(request.get_json())
     data = request.get_json()
     #data = request.get_data()
     #image_64_encode = base64.b64encode(data)
@@ -39,24 +41,79 @@ def getImage():
     message_bytes = base64.b64decode(base64_bytes)
     image_result.write(message_bytes)
     print("Image received")
-    loaded_model=load_model("C:\image\Models\I3denseAdam61.h5")
+    #loaded_model=load_model("C:\image\Models\I3denseAdam61.h5")
+    #loaded_model=load_model("C:\image\Models\I8batch128denseAdam.h5")
+    #loaded_model=load_model("C:\image\Models\model_phase_resnet50_proplus.h5")
+    
+    #loaded_model=load_model("C:\image\Models\mymodel6_71_70.h5")
+
     print("Model loaded")
     # load a single image
     #new_image = load_image("C:\image\Captures\Saved.jpg")
-    img = cv2.imread("C:\image\Captures\Saved.jpg")
-    img = cv2.resize(img,(96,96))
-    img_arr = np.asarray(img).reshape(1,96,96,3)
-    print(img_arr.shape)
-    print(type(img_arr))
-    img_arr = img_arr.astype('float32')
-    img_arr /= 255
+    #img = cv2.imread("C:\image\Captures\Saved.jpg")
+    #img = cv2.resize(img,(96,96))
+    #img_arr = np.asarray(img).reshape(1,96,96,3)
+    #img_arr = img_arr.astype('float32')
+    #img_arr /= 255
     # check prediction
-    pred = loaded_model.predict(img_arr)
-    print(EMOTIONS_LIST[np.argmax(pred)])
-    emotions = EMOTIONS_LIST[np.argmax(pred)]
+    #pred = loaded_model.predict(img_arr)
+    #print(EMOTIONS_LIST[np.argmax(pred)])
+    #emotions = EMOTIONS_LIST[np.argmax(pred)]
+    emotions = softPredict()
     response = {"emotion": emotions}
     json_dump = json.dumps(response)
     return json_dump
-       
+ 
+def softPredict():
+
+    EMOTIONS_LIST = ["Angry", "Disgust","Fear", "Happy","Neutral", "Sad","Surprise"]
+    #read the captured image
+    img = cv2.imread("C:\image\Captures\Saved.jpg")
+    
+    loaded_model_1 = load_model("C:\image\Models\I8batch128denseAdam.h5")
+    print("Loaded Efficient Net Model")
+    img_1 = cv2.resize(img,(96,96))
+    img_arr_1 = np.asarray(img_1).reshape(1,96,96,3)
+    img_arr_1 = img_arr_1.astype('float32')
+    img_arr_1 /= 255
+    pred_1 = loaded_model_1.predict(img_arr_1)
+    softVote_1 = pred_1[0][np.argmax(pred_1)]
+    print("softVote_1",softVote_1)
+    print("softVote_1 prediction",EMOTIONS_LIST[np.argmax(pred_1)])
+    
+    loaded_model_2 = load_model("C:\image\Models\model_phase_resnet50_proplus.h5")
+    print("Loaded Resnet50V2 Model")
+    img_2 = cv2.resize(img,(128,128))
+    img_arr_2 = np.asarray(img_2).reshape(1,128,128,3)
+    img_arr_2 = img_arr_2.astype('float32')
+    img_arr_2 /= 255
+    pred_2 = loaded_model_2.predict(img_arr_2)
+    softVote_2 = pred_2[0][np.argmax(pred_2)]
+    print("softVote_2",softVote_2)
+    print("softVote_2 prediction",EMOTIONS_LIST[np.argmax(pred_2)])
+
+    
+    loaded_model_3 = load_model("C:\image\Models\mymodel6_71_70.h5")
+    print("Loaded MobileNet Model")
+    pred_3 = loaded_model_3.predict(img_arr_1)
+    softVote_3 = pred_3[0][np.argmax(pred_3)]
+    print("softVote_3",softVote_3)
+    print("softVote_3 prediction",EMOTIONS_LIST[np.argmax(pred_3)])
+    
+    if( softVote_1 > softVote_2 and softVote_1 > softVote_3):
+        return EMOTIONS_LIST[np.argmax(pred_1)]
+    
+    if( softVote_2 > softVote_1 and softVote_2 > softVote_3):
+        return EMOTIONS_LIST[np.argmax(pred_2)]
+    
+    if( softVote_3 > softVote_1 and softVote_3 > softVote_2):
+        return EMOTIONS_LIST[np.argmax(pred_3)]
+        
+    
+    
+    
+    
+    
+ 
 if __name__ == '__main__':  
     app.run(debug = True) 
